5d8483ae20bdfe1eb5bb8dfe10a22df11e47d715
0xakash9 <devakash256@gmail.com>
2024-07-15 22:27:20 +0530

    fix: linting issues

--- rspack.config.js
+++ rspack.config.js
@@ -3,38 +3,38 @@
 const path = require("path");
 
 const extensionConfig = {
-  target: "node",
-  mode: "none",
+	target: "node",
+	mode: "none",
 
-  entry: "./src/extension.ts",
-  output: {
-    // the bundle is stored in the 'dist' folder (check package.json)
-    path: path.resolve(__dirname, "dist"),
-    filename: "extension.js",
-    libraryTarget: "commonjs2",
-  },
-  externals: {
-    vscode: "commonjs vscode",
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "builtin:swc-loader",
-          },
-        ],
-      },
-    ],
-  },
-  devtool: "nosources-source-map",
-  infrastructureLogging: {
-    level: "log", // enables logging required for problem matchers
-  },
+	entry: "./src/extension.ts",
+	output: {
+		// the bundle is stored in the 'dist' folder (check package.json)
+		path: path.resolve(__dirname, "dist"),
+		filename: "extension.js",
+		libraryTarget: "commonjs2",
+	},
+	externals: {
+		vscode: "commonjs vscode",
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+					},
+				],
+			},
+		],
+	},
+	devtool: "nosources-source-map",
+	infrastructureLogging: {
+		level: "log", // enables logging required for problem matchers
+	},
 };
 module.exports = [extensionConfig];
